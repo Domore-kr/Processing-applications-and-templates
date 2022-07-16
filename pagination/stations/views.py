ccda202899e7fc6a2eb16556a13966cff3cf6135
Domore-kr <8749132@mail.ru>
2022-07-16 16:09:43 +0500

    Refactoring

--- pagination/stations/views.py
+++ pagination/stations/views.py
@@ -5,6 +5,8 @@ from django.urls import reverse
 from pagination.settings import BUS_STATION_CSV
 import csv
 
+with open(BUS_STATION_CSV, newline='') as f:
+    CONTENT = [dict(i) for i in csv.DictReader(f)]
 
 def index(request):
     return redirect(reverse('bus_stations'))
@@ -13,11 +15,10 @@ def index(request):
 def bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
-    with open(BUS_STATION_CSV, newline='') as f:
-        CONTENT = [dict(i) for i in csv.DictReader(f)]
     page_number = int(request.GET.get('page', 1))
     paginator = Paginator(CONTENT, 10)
     page = paginator.get_page(page_number)
+    print(page.object_list)
     context = {
         'bus_stations': page,
         'page': page,
